63061c941dbe66f256dd61ce0558613c8190fc68
victor <victorangelograca@hotmail.com>
2021-01-28 12:47:23 -0800

    finished

--- src/fork.cpp
+++ src/fork.cpp
@@ -8,14 +8,51 @@
 // pid == 0, son
 // pid > 0, father
 
-std::string timeToStr(std::time_t *t) {
-    if (nullptr == t) {
-        auto now = std::chrono::system_clock::now();
-        std::time_t time = std::chrono::system_clock::to_time_t(now);
-        t = &time;
-    }
+void lastNode(const std::string &nodeName, int nodeTTL);
 
-    std::tm *now = std::localtime(t);
+void
+intermediateNode(const std::string &nodeName, const std::string &childName, int childBirthTime,
+                 int childTTL, int nodeTTL,
+                 std::chrono::time_point<std::chrono::high_resolution_clock> parentBirth);
+
+void
+firstNode(const std::string &parentNode, const std::string &nodeName, const std::string &childName,
+          int childBirthTime, int childTTL, int parentTTL, int nodeTTL,
+          std::chrono::time_point<std::chrono::high_resolution_clock> parentBirth);
+
+void fathersAge(std::chrono::time_point<std::chrono::high_resolution_clock> birth);
+
+std::string timeToStr(std::chrono::time_point<std::chrono::high_resolution_clock> time);
+
+void
+kill(const std::string &nodeName, std::chrono::time_point<std::chrono::high_resolution_clock> birth);
+
+int main() {
+    std::chrono::time_point<std::chrono::high_resolution_clock> fathersBirth = std::chrono::system_clock::now();
+    std::cout << "+ fathers birth: " << timeToStr(fathersBirth) << '\n';
+
+    sleep(14);
+    fathersAge(fathersBirth); // 14
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        std::cout << "error when creating " << "child 1" << "\n";
+        exit(-1);
+    } else if (pid == 0) {
+        intermediateNode("child 1", "grandchild 1", 12, 18, 18, fathersBirth);
+    } else {
+        sleep(2);
+        fathersAge(fathersBirth); // 16
+
+        firstNode("father", "child 2", "grandchild 2", 14, 12, 44, 16, fathersBirth);
+    }
+}
+
+std::string timeToStr(std::chrono::time_point<std::chrono::high_resolution_clock> time) {
+    std::time_t t = std::chrono::system_clock::to_time_t(time);
+
+    std::tm *now = std::localtime(&t);
 
     std::ostringstream s;
     s << now->tm_hour << ':'
@@ -25,134 +62,70 @@ std::string timeToStr(std::time_t *t) {
     return s.str();
 }
 
-int main() {
-    // father
+void fathersAge(std::chrono::time_point<std::chrono::high_resolution_clock> birth) {
+    std::chrono::time_point<std::chrono::high_resolution_clock> death = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = death - birth;
 
-    auto fathersBirth = std::chrono::system_clock::now();
-    std::time_t fathersBirthTime = std::chrono::system_clock::to_time_t(fathersBirth);
-    std::cout << "+ fathers birth: " << timeToStr(&fathersBirthTime) << '\n';
+    std::cout << "father has " << int(elapsed_seconds.count()) << " years\n";
+}
 
-    sleep(14);
-    std::cout << "father has 14 years\n";
+void firstNode(const std::string &parentNode, const std::string &nodeName, const std::string &childName,
+               const int childBirthTime, const int childTTL, const int parentTTL, const int nodeTTL,
+               std::chrono::time_point<std::chrono::high_resolution_clock> parentBirth) {
+    pid_t pid = fork();
 
-    pid_t fatherPid = fork();
-
-    if (fatherPid < 0) {
-        std::cout << "error when creating child 1\n";
+    if (pid < 0) {
+        std::cout << "error when creating " << nodeName << "\n";
         exit(-1);
-    } else if (fatherPid == 0) {
-        // son 1
-
-        auto son1Birth = std::chrono::system_clock::now();
-        std::time_t son1BirthTime = std::chrono::system_clock::to_time_t(son1Birth);
-        std::cout << "+ son 1 birth: " << timeToStr(&son1BirthTime) << '\n';
-
-        sleep(12);// son 1 has 12
-        std::cout << "father has 26 years\n";
-
-        pid_t son1Pid = fork();
-
-        if (son1Pid < 0) {
-            std::cout << "error when creating grandchild 1\n";
-            exit(-1);
-        } else if (son1Pid == 0) {
-            // grandchild 1
-
-            auto gc1Birth = std::chrono::system_clock::now();
-            std::time_t gc1BirthTime = std::chrono::system_clock::to_time_t(gc1Birth);
-            std::cout << "+ grandchild 1 birth: " << timeToStr(&gc1BirthTime) << '\n';
-
-            sleep(18);   // grandchildren 1 has 18
-
-            auto gc1Death = std::chrono::system_clock::now();
-            std::chrono::duration<double> elapsed_seconds = gc1Death - gc1Birth;
-            std::time_t end_time = std::chrono::system_clock::to_time_t(gc1Death);
-
-            std::cout << "- grandchild 1 died: " << timeToStr(&end_time)
-                      << " --- grandchild 1 lived: " << elapsed_seconds.count() << "s\n";
-
-            exit(0);
-        } else {
-            // son 1
-            sleep(18);  // son 1 has 30
-
-            auto son1Death = std::chrono::system_clock::now();
-            std::chrono::duration<double> elapsed_seconds = son1Death - son1Birth;
-            std::time_t end_time = std::chrono::system_clock::to_time_t(son1Death);
-
-            std::cout << "- son 1 died: " << timeToStr(&end_time)
-                      << " --- son 1 lived: " << elapsed_seconds.count() << "s\n";
-
-            exit(1);
-        }
+    } else if (pid == 0) {
+        intermediateNode(nodeName, childName, childBirthTime, childTTL, nodeTTL, parentBirth);
     } else {
         // father
-        sleep(2);   // father has 16 years
-        std::cout << "father has 16 years\n";
-
-        pid_t son2Pid = fork();
-
-        if (son2Pid < 0) {
-            std::cout << "error when creating son 2\n";
-            exit(-1);
-        } else if (son2Pid == 0) {
-            // son 2
-            auto son2Birth = std::chrono::system_clock::now();
-            std::time_t son2BirthTime = std::chrono::system_clock::to_time_t(son2Birth);
-            std::cout << "+ son 2 birth: " << timeToStr(&son2BirthTime) << '\n';
-
-
-            sleep(14);   // son 2 has 14
-            std::cout << "father has 30 years\n";
-
-            pid_t gc2Pid = fork();
-
-            if (gc2Pid < 0) {
-                std::cout << "error when creating grandchild 2\n";
-                exit(-1);
-            } else if (gc2Pid == 0) {
-                // grandchild 2
-
-                auto gc2Birth = std::chrono::system_clock::now();
-                std::time_t gc2BirthTime = std::chrono::system_clock::to_time_t(gc2Birth);
-                std::cout << "+ grandchild 2 birth: " << timeToStr(&gc2BirthTime) << '\n';
-
-                sleep(12);   //grandchildren 2 has 12
-
-                auto gc2Death = std::chrono::system_clock::now();
-                std::chrono::duration<double> elapsed_seconds = gc2Death - gc2Birth;
-                std::time_t end_time = std::chrono::system_clock::to_time_t(gc2Death);
-
-                std::cout << "- grandchild 2 died: " << timeToStr(&end_time)
-                          << " --- grandchild 2 lived: " << elapsed_seconds.count() << "s\n";
-
-                exit(0);
-            } else {
-                // son 2
-                sleep(16);   // son 2 has 30
-
-                auto son2Death = std::chrono::system_clock::now();
-                std::chrono::duration<double> elapsed_seconds = son2Death - son2Birth;
-                std::time_t end_time = std::chrono::system_clock::to_time_t(son2Death);
-
-                std::cout << "- son 2 died: " << timeToStr(&end_time)
-                          << " --- son 2 lived: " << elapsed_seconds.count() << "s\n";
-
-                exit(1);
-            }
-        } else {
-            // father
-            sleep(44);   // father has 16 years
-            std::cout << "father has 60 years\n";
-
-            auto fatherDeath = std::chrono::system_clock::now();
-            std::chrono::duration<double> elapsed_seconds = fatherDeath - fathersBirth;
-            std::time_t end_time = std::chrono::system_clock::to_time_t(fatherDeath);
-
-            std::cout << "- father died: " << timeToStr(&end_time)
-                      << " --- father lived: " << elapsed_seconds.count() << "s\n";
-
-            exit(1);
-        }
+        sleep(parentTTL);
+        fathersAge(parentBirth);  // 60 years
+        kill(parentNode, parentBirth);
+        exit(1);
     }
+}
+
+void intermediateNode(const std::string &nodeName, const std::string &childName, const int childBirthTime,
+                      const int childTTL, const int nodeTTL,
+                      std::chrono::time_point<std::chrono::high_resolution_clock> parentBirth) {
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> birth = std::chrono::system_clock::now();
+    std::cout << "+ " << nodeName << " birth: " << timeToStr(birth) << '\n';
+
+    sleep(childBirthTime);
+    fathersAge(parentBirth); // 26
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        std::cout << "error when creating " << childName << "\n";
+        exit(-1);
+    } else if (pid == 0) {
+        lastNode(childName, childTTL);
+
+        exit(0);
+    } else {
+        sleep(nodeTTL);
+        kill(nodeName, birth);
+        exit(1);
+    }
+}
+
+void kill(const std::string &nodeName, const std::chrono::time_point<std::chrono::high_resolution_clock> birth) {
+    std::chrono::time_point<std::chrono::high_resolution_clock> death = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = death - birth;
+
+    std::cout << "- " << nodeName << " died: " << timeToStr(death)
+              << " --- " << nodeName << " lived: " << int(elapsed_seconds.count()) << "s\n";
+}
+
+void lastNode(const std::string &nodeName, const int nodeTTL) {
+    std::chrono::time_point<std::chrono::high_resolution_clock> birth = std::chrono::system_clock::now();
+    std::cout << "+ " << nodeName << " birth: " << timeToStr(birth) << '\n';
+
+    sleep(nodeTTL);
+    kill(nodeName, birth);
 }
